b07bf74b04d8c64b6274b9f54ccee543abb48429
skhapijulhossen <skhapijulhossen@gmail.com>
2024-01-15 21:32:16 +0530

    update on feature pipeline

--- run_feature_pipeline.py
+++ run_feature_pipeline.py
@@ -2,20 +2,24 @@ from zenml import pipeline
 import logging
 from steps.ingest import ingest_data
 from steps.clean import clean_data
+from steps.encode import encode_data
+from steps.split import split_data
 import config
 
 
 @pipeline
-def run_pipeline() -> None:
+def run_feature_pipeline() -> None:
     """
     Pipeline that runs the data ingestion and data cleaning steps.
     """
     global ingest_data, clean_data
     try:
-        logging.info("Running data ingestion and cleaning pipeline...")
+        logging.info("Running Feature pipeline...")
         data = ingest_data(config.DATA_SOURCE)
-        # data = clean_data(data)
-        logging.info("Data ingestion and cleaning pipeline completed.")
+        data = clean_data(data)
+        data = encode_data(data)
+        train, valid, test = split_data(data)
+        logging.info("Feature pipeline completed.")
 
     except Exception as e:
         logging.error(
@@ -23,4 +27,4 @@ def run_pipeline() -> None:
 
 
 if __name__ == "__main__":
-    run_pipeline()
+    run_feature_pipeline()
