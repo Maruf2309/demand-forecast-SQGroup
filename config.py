99034d55971fc714886021b83baacc197137fd53
skhapijulhossen <skhapijulhossen@gmail.com>
2024-02-29 21:26:11 +0530

    train pipline fixes

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
-DATA_SOURCE = 'data/sales_BYA.csv'
+DATA_SOURCE = 'data/sales_BYA.parquet'
 ARTIFACTS_DIR = 'artifacts'
+FEATURE_STORE = 'data/features.parquet'
+MODEL_STORE = 'data/model.pkl'
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+XGB_PARAMS_SPACE = {
+    'max_depth': [3, 5, 7],
+    'learning_rate': [0.1, 0.3, 0.5],
+    'n_estimators': [100, 200, 300],
+    'subsample': [0.5, 0.7, 0.9],
+    'colsample_bytree': [0.5, 0.7, 0.9],
+    'gamma': [0, 0.1, 0.2]
+}
