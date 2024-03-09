6d3d1a88bd8b250105a60bf26405e01fc578e637
skhapijulhossen <skhapijulhossen@gmail.com>
2024-03-09 15:21:49 +0530

    minor fixes

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import config
-from pipelines.ETLPipeline import ETLFeaturePipeline
+from pipelines.featurePipeline import FeaturePipeline
 from pipelines.trainPipeline import trainModelPipeline
 
 
 if __name__ == '__main__':
-    features = ETLFeaturePipeline(data_source=config.DATA_SOURCE,)
+    features = FeaturePipeline(data_source=config.DATA_SOURCE,)
     train = trainModelPipeline(data_source=config.FEATURE_STORE)
